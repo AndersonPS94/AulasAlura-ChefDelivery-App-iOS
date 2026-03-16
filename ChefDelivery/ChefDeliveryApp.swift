a47b54f250d2f34f010a04c11831efcadf2652cd
Anderson Santos <dson_adn@hotmail.com>
2026-03-16 20:07:34 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Anderson Pereira Dos Santos on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Anderson Pereira Dos Santos on 16/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
